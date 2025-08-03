408c812068a6117fc838c7a94e66dba9e85b3ef8
alreadydonewithu <alreadydonewithu@gmail.com>
2025-08-04 01:58:40 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-SS_PASSWORD="${PASSWORD}"
+SS_PASSWORD="${PASSWORD:-fardad1386}"
 SS_METHOD="${ENCRYPT:-aes-256-gcm}"
 WS_PATH="${WS_PATH:-/vpn}"
+SERVER_HOST="${RENDER_EXTERNAL_HOSTNAME:-0.0.0.0}"
 
-exec ss-server \
-  -s 0.0.0.0 -p 443 \
-  -k "${SS_PASSWORD}" -m "${SS_METHOD}" \
+exec ssserver \
+  -s 0.0.0.0 \
+  -p 443 \
+  -k "${SS_PASSWORD}" \
+  -m "${SS_METHOD}" \
   --plugin v2ray-plugin \
-  --plugin-opts "server;tls;host=${RENDER_INTERNAL_HOST};path=${WS_PATH}"
+  --plugin-opts "server;tls;host=${SERVER_HOST};path=${WS_PATH}"
